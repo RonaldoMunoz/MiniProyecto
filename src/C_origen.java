ac58d27a1aaeec6d0b0dd7b4d7c96a3e4ab6827a
RonaldoMunoz <yeifer.munoz@correounivalle.edu.co>
2023-09-29 01:14:10 -0500

    Plantilla miniproyecto

---
+++ src/C_origen.java
@@ -0,0 +1,6 @@
+public enum C_origen {
+    Andalucía, Bugalagrande, El_Cerrito, Ginebra, Guacarí, Buga, Riofrío, Trujillo, Calima, Darién, Restrepo, San_Pedro,
+    Tuluá, Yotoco, El_Águila, Alcalá, Ansermanuevo, Argelia, El_Cairo, La_Unión, La_Victoria, Ulloa, Bolívar, Cartago,
+    el_Dovio, Obando, Roldanillo, Toro, Versalles, Zarzal, Buenaventura, Caicedonia, Sevilla, Cali, Candelaria, Palmira,
+    Dagua, Florida, Jamundí, la_Cumbre, Pradera, Vijes, Yumbo
+}
---
+++ src/Candidato.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Candidato extends Ciudadano {
+
+        private Partido_p partido_politico;
+        private List<String> promesas;
+}
