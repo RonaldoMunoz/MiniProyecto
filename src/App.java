cb67cc4d98c422543866ef5a576926049c6c768b
MaicolRodriguez12 <michael.arana@correounivalle.edu.co>
2023-10-10 17:55:18 -0500

    menu parte 3 consola

--- src/App.java
+++ src/App.java
@@ -5,43 +5,43 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class App {
-  public static void main(String[] args) throws Exception {
-    Menu m1 = new Menu();
-    int size = 0;
-    Scanner opcionesm1 = new Scanner(System.in);
-    Scanner sizeA = new Scanner(System.in);
-    m1.cleanScreen();
-    while (true) {
-      try {
-        System.out.println("Ingresa la cantidad de candidatos que vas a ingresar:");
-        size = sizeA.nextInt();
-        break; // Salir del bucle si la entrada es válida
-      } catch (InputMismatchException e) {
-        sizeA.nextLine(); // Limpiar el búfer de entrada
-      }
-    }
-
-    Candidato candidato[] = new Candidato[size];
-    Menu1op option1 = null;
-    m1.cleanScreen();
-    boolean end = false;
-    String opt;
-
-    do {
-      while (true) {
-        try {
-          m1.menu1();
-          opt = opcionesm1.nextLine().toUpperCase();
-          option1 = Menu1op.valueOf(opt);
-          break;
-        } catch (IllegalArgumentException e) {
-          System.out.println("Seleccione una opcion valida");
-          opcionesm1.nextLine();
+    public static void main(String[] args) throws Exception {
+        Menu m1 = new Menu();
+        int size = 0;
+        Scanner opcionesm1 = new Scanner(System.in);
+        Scanner sizeA = new Scanner(System.in);
+        m1.cleanScreen();
+        while (true) {
+            try {
+                System.out.println("Ingresa la cantidad de candidatos que vas a ingresar:");
+                size = sizeA.nextInt();
+                break; // Salir del bucle si la entrada es válida
+            } catch (InputMismatchException e) {
+                sizeA.nextLine(); // Limpiar el búfer de entrada
+            }
         }
-      }
-      switch (option1) {
-        case A:
-          m1.insertCandidato(candidato);
+
+        Candidato candidato[] = new Candidato[size];
+        Menu1op option1 = null;
+        m1.cleanScreen();
+        boolean end = false;
+        String opt;
+
+        do {
+            while (true) {
+                try {
+                    m1.menu1();
+                    opt = opcionesm1.nextLine().toUpperCase();
+                    option1 = Menu1op.valueOf(opt);
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Seleccione una opcion valida");
+                    opcionesm1.nextLine();
+                }
+            }
+            switch (option1) {
+                case A:
+                    m1.insertCandidato(candidato);
 
                     break;
                 case B:
@@ -101,17 +101,19 @@ public class App {
                     System.out.println("Ingrese una opcion valida");
             }
         } while (end == false);
+        // MENU PARTE 3
+        Scanner menu3 = new Scanner(System.in);
+        int opcionMenu3;
 
-        m1.menu2(candidato); /* Puede modificarlo */
-        // Parte 3 de aqui para bajo
-        for (int i = 0; i < candidato.length; i++) { /*
-                                                      * Forma brusca de mostrar los datos de los candidatos puede ser
-                                                      * mejorado
-                                                      */
-            if (candidato[i] != null) {
-                candidato[i].listarDatosVotos();
-            }
-        }
+        do {
+            System.out.println("\n Menú de Opciones: \n");
+            System.out.println("[1] <- Ingresar número de votos \n");
+            System.out.println("[2] <- Mostrar resultados \n");
+            System.out.println("[3] <- Mostrar resultados del partido con más candidatos inscritos \n");
+            System.out.println("[4] <- Salir \n");
 
+            opcionMenu3 = menu3.nextInt();
+
+        } while (opcionMenu3 != 4);
     }
 }
